547de14299b14d303e963ca19e1e0270a31d1d24
karendutoit <karen_dutoit_uk@yahoo.com>
2019-10-02 11:43:37 +0100

    ccc

--- pizza-program.js
+++ pizza-program.js
@@ -26,25 +26,25 @@ app.get('/karen-mservice-repo', (req, resp) => {
     resp.send(pizzas)
 });
 
-app.get('/karen-mservice-repo/*', (req, resp) => {
+app.get('/karen-mservice-repo/:id([0-9]+)', (req, resp) => {
     // get the id form the url path
-    const id = parseInt(req.params[0]);
+    const id = parseInt(req.params['id']);
 
     // log which endpoint was hit
-    console.log('Endpoint /karen-mservice-repo/${id} hit');
+    console.log(`Endpoint /karen-mservice-repo/${id} hit`);
 
     // find pizza with the id in the request
     const pizza = pizzas.find(pizza => pizza.id === id);
 
     // set the content type header of the response
-    resp.setHeader('Content-type', 'application/json');
+    resp.setHeader('Content-Type', 'application/json');
     // if pizza found return it else error message
     // with status 404 not found
     if (pizza) {
         resp.send(pizza);
     }
     else {
-        resp.status(404).send('{error:"pizza with ID${id} nor found"} ');
+        resp.status(404).send({ error: `pizza with ID ${id} not found` });
     }
 });
 
